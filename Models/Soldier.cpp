95b00d1aab538731e80edf7de86d661cd0cd4b9f
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-11-04 14:54:15 +0300

    Created Soldier Model

---
+++ Models/Soldier.cpp
@@ -0,0 +1,52 @@
+#include "Soldier.h"
+
+Soldier::Soldier(QObject *parent) : QObject(parent)
+{
+
+}
+
+Soldier::Soldier(int id, QString name, Rank rank, Role role, ShiftTime shiftTime, QObject *parent)
+    :   QObject(parent), soldierId(id), soldierName(name), soldierRank(rank), soldierRole(role), soldierShiftTime(shiftTime)
+{
+
+}
+
+int Soldier::GetId() const
+{
+    return soldierId;
+}
+
+QString Soldier::GetName() const
+{
+    return soldierName;
+}
+
+Soldier::Rank Soldier::GetRank() const
+{
+    return soldierRank;
+}
+
+void Soldier::SetRank(Rank rank)
+{
+    soldierRank = rank;
+}
+
+Soldier::Role Soldier::GetRole() const
+{
+    return soldierRole;
+}
+
+void Soldier::SetRole(Role role)
+{
+    soldierRole = role;
+}
+
+Soldier::ShiftTime Soldier::GetShiftTime() const
+{
+    return soldierShiftTime;
+}
+
+void Soldier::SetShiftTime(ShiftTime shiftTime)
+{
+    soldierShiftTime = shiftTime;
+}
