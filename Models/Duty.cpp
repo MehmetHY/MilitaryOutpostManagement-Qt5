b51b4835e42b73deeabd0f9aaea03dd716d40ff6
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-11-13 14:20:20 +0300

    rework duty model

--- Models/Duty.cpp
+++ Models/Duty.cpp
@@ -1,6 +1,45 @@
 #include "duty.h"
 
-Duty::Duty()
-{
+Duty::Duty(const QString &name, Soldier *soldier, const QDateTime &startDate, const QDateTime &endDate)
+    : name(name), soldier(soldier), startDate(startDate), endDate(endDate)
+{}
 
+Soldier *Duty::getSoldier() const
+{
+    return soldier;
+}
+
+void Duty::setSoldier(Soldier *newSoldier)
+{
+    soldier = newSoldier;
+}
+
+const QDateTime &Duty::getStartDate() const
+{
+    return startDate;
+}
+
+void Duty::setStartDate(const QDateTime &newStartDate)
+{
+    startDate = newStartDate;
+}
+
+const QDateTime &Duty::getEndDate() const
+{
+    return endDate;
+}
+
+void Duty::setEndDate(const QDateTime &newEndDate)
+{
+    endDate = newEndDate;
+}
+
+const QString &Duty::getName() const
+{
+    return name;
+}
+
+void Duty::setName(const QString &newName)
+{
+    name = newName;
 }
--- Models/Duty.h
+++ Models/Duty.h
@@ -1,11 +1,28 @@
 #ifndef DUTY_H
 #define DUTY_H
 
+#include <QString>
+#include <QDateTime>
 
 class Duty
 {
+private:
+    QString name;
+    class Soldier* soldier;
+    QDateTime startDate;
+    QDateTime endDate;
+
 public:
-    Duty();
+    Duty(const QString &name, Soldier *soldier, const QDateTime &startDate, const QDateTime &endDate);
+
+    const QString &getName() const;
+    void setName(const QString &newName);
+    Soldier *getSoldier() const;
+    void setSoldier(Soldier *newSoldier);
+    const QDateTime &getStartDate() const;
+    void setStartDate(const QDateTime &newStartDate);
+    const QDateTime &getEndDate() const;
+    void setEndDate(const QDateTime &newEndDate);
 };
 
 #endif // DUTY_H
