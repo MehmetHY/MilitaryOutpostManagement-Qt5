ca7660c189c2427e77603a1d0d36b2c328574eb6
MehmetHY <mehmethakkiyildiz@yahoo.com>
2021-11-13 14:15:57 +0300

    rework platoon model

--- Models/Platoon.cpp
+++ Models/Platoon.cpp
@@ -1,6 +1,24 @@
 #include "platoon.h"
 
-Platoon::Platoon()
-{
+Platoon::Platoon(const QString &name) : name(name)
+{}
 
+const QList<Squad *> &Platoon::getSquads() const
+{
+    return squads;
+}
+
+const QList<Duty *> &Platoon::getDuties() const
+{
+    return duties;
+}
+
+const QString &Platoon::getName() const
+{
+    return name;
+}
+
+void Platoon::setName(const QString &newName)
+{
+    name = newName;
 }
--- Models/Platoon.h
+++ Models/Platoon.h
@@ -1,11 +1,22 @@
 #ifndef PLATOON_H
 #define PLATOON_H
 
+#include <QString>
+#include <QList>
 
 class Platoon
 {
+private:
+    QString name;
+    QList<class Squad*> squads;
+    QList<class Duty*> duties;
 public:
-    Platoon();
+    Platoon(const QString &name);
+
+    const QString &getName() const;
+    void setName(const QString &newName);
+    const QList<Squad *> &getSquads() const;
+    const QList<Duty *> &getDuties() const;
 };
 
 #endif // PLATOON_H
